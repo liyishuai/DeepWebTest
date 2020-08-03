0d3a141e74c76e81ee96ac1689ff6ad5d0202c92
Yishuai Li <yishuai@upenn.edu>
2020-08-02 22:18:53 -0400

    Add TCP model

--- coq/Switch.v
+++ coq/Switch.v
@@ -1,4 +1,5 @@
 From Coq Require Import
+     Nat
      List.
 From ExtLib Require Import
      Extras
@@ -14,6 +15,7 @@ Import
   FunctorNotation
   ListNotations
   MonadNotation.
+Open Scope bool_scope.
 Open Scope monad_scope.
 
 Definition udp {E} `{nondetE -< E} `{netE -< E} : itree E void :=
@@ -30,6 +32,31 @@ Definition udp {E} `{nondetE -< E} `{netE -< E} : itree E void :=
                                   or (ret (pkt :: in_pkt, out_pkt))
                                      (ret (in_pkt, pkt :: out_pkt)))
                                in_pkt1 (ret ([], []));;
-     fold_left (fun _ pkt =>
-                  trigger (Net__Send pkt)) out_pkt2 (ret tt);;
+     fold_left (fun _ pkt => trigger (Net__Send pkt)) out_pkt2 (ret tt);;
+     call in_pkt2) [].
+
+Definition tcp {E} `{nondetE -< E} `{netE -< E} : itree E void :=
+  (rec-fix loop in_pkt0 :=      (* Head of every list came latest. *)
+     conns <- trigger Net__Select;;
+     in_pkt1 <- fold_left
+               (fun in_pkt conn =>
+                  pkt <- embed Net__Recv conn;;
+                  cons pkt <$> in_pkt)
+               conns (ret in_pkt0);;
+     '(in_pkt2, out_pkt2, _) <-
+       fold_right
+         (fun pkt i_o_f =>
+            '(in_pkt, out_pkt, f) <- i_o_f;;
+            if f pkt : bool
+            then
+              or (ret (pkt :: in_pkt, out_pkt,
+                       fun pkt' =>
+                         if (packet__src pkt' =? packet__src pkt) &&
+                            (packet__dst pkt' =? packet__dst pkt)
+                         then false
+                         else f pkt'))
+                 (ret (in_pkt, pkt :: out_pkt, f))
+            else ret (pkt :: in_pkt, out_pkt, f))
+         (ret ([], [], const true)) in_pkt1;;
+     fold_right (fun pkt _ => trigger (Net__Send pkt)) (ret tt) out_pkt2;;
      call in_pkt2) [].
